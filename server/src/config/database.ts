98d505274502aee116cdcfc85478e5c888513777
khemu <alghost2004@gmail.com>
2024-07-09 21:14:27 +0300

    .

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -3,10 +3,6 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   if (mongoose.connection.readyState > 1) return;
 
-  mongoose.connection.on("connected", () => {
-    console.log("MongoDB Connected");
-  });
-
   try {
     await mongoose.connect(process.env.MONGODB_URI || "none", {
       dbName: "Omni_Store",
